cee195e93c71a4dc028624a3ec044c42997003e0
Leoh18 <leonardobernardi56@gmail.com>
2023-02-07 16:02:43 -0300

    Fonte base api

---
+++ src/demo/hanoiTower.ts
@@ -0,0 +1,4 @@
+/* regras
+1- nao pode se ter nenhum disco com valor maior que ele mesmo em cima
+2-
+*/
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+console.log(`${Math.random()*100}`)
+const hanoi = {
+    a: [3,2,1],
+    b: [],
+    c: []
+}
+
+let userInput = prompt(`test`)
+console.log(userInput);
+
+
+let peso = hanoi.a.pop()
+
+hanoi.a.
